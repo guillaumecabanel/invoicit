a1f364385134bab1e1a98ca0650db702137856a1
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2017-08-26 18:06:09 +0200

    config money format

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,3 +1,42 @@
 MoneyRails.configure do |config|
+  # To set the default currency
+  #
   config.default_currency = :eur
+
+  # Register a custom currency
+  #
+  # Example:
+  # config.register_currency = {
+  #   :priority            => 1,
+  #   :iso_code            => "EU4",
+  #   :name                => "Euro with subunit of 4 digits",
+  #   :symbol              => "€",
+  #   :symbol_first        => true,
+  #   :subunit             => "Subcent",
+  #   :subunit_to_unit     => 10000,
+  # }
+
+  # Specify a rounding mode
+  # Any one of:
+  #
+  # BigDecimal::ROUND_UP,
+  # BigDecimal::ROUND_DOWN,
+  # BigDecimal::ROUND_HALF_UP,
+  # BigDecimal::ROUND_HALF_DOWN,
+  # BigDecimal::ROUND_HALF_EVEN,
+  # BigDecimal::ROUND_CEILING,
+  # BigDecimal::ROUND_FLOOR
+  #
+  # set to BigDecimal::ROUND_HALF_EVEN by default
+  #
+  # config.rounding_mode = BigDecimal::ROUND_HALF_UP
+
+  # Set default money format globally.
+  # Default value is nil meaning "ignore this option".
+  #
+  # \u00a0 is the non-breaking space char
+  config.default_format = {
+    decimal_mark: ",",
+    thousands_separator: "\u00a0"
+  }
 end
