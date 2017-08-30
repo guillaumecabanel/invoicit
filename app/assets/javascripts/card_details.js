f5148dbff727db12ab31bb6414920957db98c10f
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2017-08-30 09:53:12 +0200

    temporary fix with jquery to toggle details in invoice card

--- app/assets/javascripts/card_details.js
+++ app/assets/javascripts/card_details.js
@@ -1,14 +1,21 @@
-const cards = document.body.querySelectorAll('.invoice-card');
-
-cards.forEach((card) => {
-  const details = card.querySelector('.box-details');
-  const box = card.querySelector('.box-has-details');
-  const caret = card.querySelector('span.caret-right i')
-
-  box.addEventListener("click", (event) => {
-    details.classList.toggle("hidden");
-    details.classList.toggle("swing-in-top-fwd");
-    caret.classList.toggle("has-text-grey-light");
-    caret.classList.toggle("rotate-90-bl-cw-on");
+$(document).ready(function(){
+  $('.invoice-card .box-has-details').on("click", function(){
+    $(this).parent().find('.box-details').slideToggle();
   });
 });
+
+
+// const cards = document.body.querySelectorAll('.invoice-card');
+//
+// cards.forEach((card) => {
+//   const details = card.querySelector('.box-details');
+//   const box = card.querySelector('.box-has-details');
+//   const caret = card.querySelector('span.caret-right i')
+//
+//   box.addEventListener("click", (event) => {
+//     details.classList.toggle("hidden");
+//     details.classList.toggle("swing-in-top-fwd");
+//     caret.classList.toggle("has-text-grey-light");
+//     caret.classList.toggle("rotate-90-bl-cw-on");
+//   });
+// });
