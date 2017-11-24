77ff5eb97b239066e2d04360999368b1da970649
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2017-11-24 17:38:49 +0100

    fix js

--- app/assets/javascripts/card_details.js
+++ app/assets/javascripts/card_details.js
@@ -4,15 +4,30 @@
 //   });
 // });
 
+//
+// const cards = document.body.querySelectorAll('.invoice-card');
+//
+// cards.forEach((card) => {
+//   const details = card.querySelector('.box-details');
+//   const box = card.querySelector('.box-has-details');
+//   const caret = card.querySelector('span.caret-right i')
+//
+//   box.addEventListener("click", (event) => {
+//     details.classList.toggle("hidden");
+//     details.classList.toggle("swing-in-top-fwd");
+//     caret.classList.toggle("has-text-grey-light");
+//     caret.classList.toggle("rotate-90-bl-cw-on");
+//   });
+// });
 
-const cards = document.body.querySelectorAll('.invoice-card');
+var cards = document.body.querySelectorAll('.invoice-card');
 
-cards.forEach((card) => {
-  const details = card.querySelector('.box-details');
-  const box = card.querySelector('.box-has-details');
-  const caret = card.querySelector('span.caret-right i')
+cards.forEach(function(card) {
+  var details = card.querySelector('.box-details');
+  var box = card.querySelector('.box-has-details');
+  var caret = card.querySelector('span.caret-right i')
 
-  box.addEventListener("click", (event) => {
+  box.addEventListener("click", function(event) {
     details.classList.toggle("hidden");
     details.classList.toggle("swing-in-top-fwd");
     caret.classList.toggle("has-text-grey-light");
