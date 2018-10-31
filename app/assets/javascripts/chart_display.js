3cab18476cf1e21e1922af92c6b30fc66a8b40f0
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2018-10-31 12:05:58 +0100

    const not supported, using classique function in js

--- app/assets/javascripts/chart_display.js
+++ app/assets/javascripts/chart_display.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-  const chart = document.querySelector('.chart-container');
+  var chart = document.querySelector('.chart-container');
 
   if (chart) {
-    const chartLegends = document.querySelectorAll('.percentage-chart + .chart-legend');
+    var chartLegends = document.querySelectorAll('.percentage-chart + .chart-legend');
     chartLegends.forEach(function(chartLegend) {
       chartLegend.setAttribute('transform', "translate(50, 100)");
     })
